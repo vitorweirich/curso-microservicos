c3f9e0862899e3d3e20560fc66874ff6c75f4e53
Vitor Mateus Weirich <vitor.weirich@pampa.compasso>
2021-04-07 11:26:22 -0300

    Criando projeto hr-config-server

---
+++ hr-config-server/src/main/java/com/devsuperior/grconfigserver/HrConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.grconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrConfigServerApplication.class, args);
+	}
+
+}
