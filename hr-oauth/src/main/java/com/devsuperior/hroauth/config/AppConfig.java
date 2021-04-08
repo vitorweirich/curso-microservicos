bd7a7eda495eca6bdca84b6d23f89d0b6e0dba69
Vitor Mateus Weirich <vitor.weirich@pampa.compasso>
2021-04-08 16:31:19 -0300

    Configurando as configurações globais no servidor de configurações

--- hr-oauth/src/main/java/com/devsuperior/hroauth/config/AppConfig.java
+++ hr-oauth/src/main/java/com/devsuperior/hroauth/config/AppConfig.java
@@ -1,5 +1,6 @@
 package com.devsuperior.hroauth.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -8,6 +9,9 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @Configuration
 public class AppConfig {
+	
+	@Value("${jwt.secret}")
+	private String jwtSecret;
 
 	@Bean
 	public BCryptPasswordEncoder bCryptPasswordEncoder() {
@@ -17,7 +21,7 @@ public class AppConfig {
 	@Bean
 	public JwtAccessTokenConverter jwtAccessTokenConverter() {
 		JwtAccessTokenConverter jwt = new JwtAccessTokenConverter();
-		jwt.setSigningKey("MY-SECRET-KEY");
+		jwt.setSigningKey(jwtSecret);
 		return jwt;
 	}
 	
